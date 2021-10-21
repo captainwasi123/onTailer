665eac26907e21b5af82f28179201fb8783922c7
unknown <anasqamar.work@gmail.com>
2021-10-21 13:57:52 -0700

    update by anas 21-10-21 last update today

--- resources/views/web/includes/master2.blade.php
+++ resources/views/web/includes/master2.blade.php
@@ -4,7 +4,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title> @yield('title') | {{env('APP_NAME')}} </title>
 
-    @include('web.includes.style')
+    @include('web.includes.style2')
     @yield('addStyle')
 
 </head>
@@ -13,7 +13,7 @@
 	@include('web.includes.header2')
 	@yield('content')
 	@include('web.includes.footer')
-	@include('web.includes.script')
+	@include('web.includes.script2')
     @yield('addScript')
 
 </body>
---
+++ resources/views/web/includes/style2.blade.php
@@ -0,0 +1,15 @@
+<link href="http://fonts.cdnfonts.com/css/trochut" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=News+Cycle:wght@400;700&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Cabin:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+<!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/3.1.2/fullpage.css"/>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/3.1.2/fullpage.min.css"/> -->
+
+<link rel="stylesheet" type="text/css" href="{{URL::to('/public/website')}}/css/custom.css">
+<link rel="stylesheet" type="text/css" href="{{URL::to('/public/website')}}/css/custom2.css">
+<link rel="stylesheet" type="text/css" href="{{URL::to('/public/website')}}/css/slick.css"/>
+<link rel="stylesheet" type="text/css" href="{{URL::to('/public/website')}}/css/slick-theme.css"/>
+<link rel='stylesheet' href='https://unpkg.com/xzoom/dist/xzoom.css'>
