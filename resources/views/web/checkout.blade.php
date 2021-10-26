2bf9da94ec49d0d434710fb5b82968e5efbcc614
unknown <anasqamar.work@gmail.com>
2021-10-26 11:53:26 -0700

    update by anas 26-10-21 last update today

--- resources/views/web/checkout.blade.php
+++ resources/views/web/checkout.blade.php
@@ -15,6 +15,67 @@
   .checkout-section {
     padding: 80px 0px;
 }
+.shipping-info-label
+{
+  font-weight: bold;
+  font-size: 25px;
+  line-height: 41px;
+}
+.shipping-info-content {
+    border: 1px solid #000000;
+    padding: 15px 5px;
+}
+.shipping-info-content1 {
+    border: 1px solid #000000;
+    /*padding: 15px 5px;*/
+}
+.shipping-info-content1 a {
+    background: #F0F0F0;
+    width: 100%;
+    padding: 14px 0px;
+}
+.discount-btn
+{
+  font-weight: bold;
+font-size: 25px;
+line-height: 41px;
+}
+.shipping-total h2 {
+    font-weight: bold;
+    font-size: 25px;
+    line-height: 41px;
+}
+.shipping-total h3 {
+  font-size: 25px;
+  line-height: 41px;
+}
+
+.shipping-total h4 {
+  font-size: 20px;
+  line-height: 40px;
+  letter-spacing: 0.05em;
+  text-align: right;
+}
+.shipping-total span {
+font-size: 14.5px;
+line-height: 24px;
+}
+.shipping-dash
+{
+   border-top:1px dashed black;
+}
+.checkout-btn
+{
+  font-weight: bold;
+    font-size: 20px;
+    line-height: 33px;
+    text-align: center;
+    letter-spacing: 0.02em;
+    border-radius: 0px;
+    border: 2px solid #000000;
+    padding: 4px 0px;
+    width: 100%;
+}
 </style>
 <!-- section1 start -->
   <section>
@@ -24,91 +85,117 @@
           <div class="checkout-section">
             <form action="#" method="post">
               <div class="row">
-                <div class="col-lg-6 col-md-6">
+                <div class="col-lg-6 col-md-6 pad-right-80 bor-r">
                   <div class="row">
                     <div class="col-md-12">
                       <label class="form-label personal-info-label abril-font" for="inputEmail">NAME</label>
                     </div>
+                    
                     <div class="col-md-6">
-                      <input type="email" class="form-control p-form-style1 m-t-40" id="inputEmail" placeholder="First Name" required>
-                    </div>
-                    <div class="col-md-6">
-                      <input type="email" class="form-control p-form-style1 m-t-40" id="inputEmail" placeholder="First Name" required>
-                    </div>
-                  </div>                                  
-                </div>
-                <!-- <div class="col-lg-2 col-md-1 tablet-none"></div>
-                <div class="col-lg-5 col-md-6">
-                  <div>
-                    <input type="email" class="form-control p-form-style1 m-t-80" id="inputEmail" placeholder="Last Name" required>
-                  </div>                
-                </div>
-                <style type="text/css">
-           
-                </style> -->
-                <div class="col-lg-5 col-md-6 col-6 m-t-50">
-                  <div>
-                    <label class="form-label personal-info-label abril-font" for="inputEmail">YOUR GENDER</label>
-                    <!-- <input type="email" class="form-control p-form-style1 m-t-40" id="inputEmail" placeholder="First Name" required> -->
-                    <div style="display: block; width: 100%;">
-                      <label class="radio-inline ">
-                      <input type="radio" name="optradio" checked> <span>Male</span>
-                    </label>
-                    <label class="radio-inline" style="padding-left: 50px;">
-                      <input type="radio" name="optradio"><span>Female</span>
-                    </label>
+                      <input type="text" class="form-control p-form-style1 m-t-40" id="inputEmail" placeholder="First Name" required>
                     </div>
                     
-                  </div>                
-                </div>
-                <div class="col-lg-2 col-md-1"></div>
-                <div class="col-md-5"></div>
-
-                <div class="col-lg-5 col-md-6 m-t-50">
-                  <div>
-                    <label class="form-label personal-info-label abril-font" for="inputEmail">EMAIL</label>
-                    <label class="form-label personal-info-edit news-font" for="inputEmail" style="float: right;"><a href="">&nbsp;&nbsp;|&nbsp;&nbsp;Change Password</a></label>
-                    <label class="form-label personal-info-edit news-font" for="inputEmail" style="float: right;"><a href="">Edit </a></label>
+                    <div class="col-md-6">
+                      <input type="text" class="form-control p-form-style1 m-t-40" id="inputEmail" placeholder="First Name" required>
+                    </div>
+                    
+                    <div class="col-md-12">
+                      <label class="form-label personal-info-label abril-font pad-top-50" for="inputEmail">EMAIL</label>
                     <input type="email" class="form-control p-form-style1 m-t-40" id="inputEmail" placeholder="Email" required>
-                  </div>                
-                </div>
-                <div class="col-md-2 tablet-none"></div>
+                    </div>
+                    
+                    <div class="col-md-12">
+                      <label class="form-label personal-info-label abril-font pad-top-50" for="inputEmail">MOBILE NUMBER</label>
+                      <input type="text" class="form-control p-form-style1 m-t-40" id="inputEmail" placeholder="Mobile Number" required>
+                    </div>
+                    
+                    <div class="col-md-12">
+                      <label class="form-label personal-info-label abril-font pad-top-50" for="inputEmail">ADDRESS</label>
+                      <input type="text" class="form-control p-form-style1 m-t-40" id="inputEmail" placeholder="Address" required>
+                    </div>
 
-                <div class="col-lg-5 col-md-6 m-t-50">
-                  <div>
-                    <label class="form-label personal-info-label abril-font" for="inputEmail">MOBILE NUMBER</label>
-                    <label class="form-label personal-info-edit news-font" for="inputEmail" style="float: right;"><a href="">Edit </a></label>
-                    <input type="text" class="form-control p-form-style1 m-t-40" id="inputEmail" placeholder="Mobile Number" required>
-                  </div>               
-                </div>
-
-                <div class="col-lg-5 col-md-6 m-t-50">
-                  <div>
-                    <label class="form-label personal-info-label abril-font" for="inputEmail">ADDRESS</label>
-                    <label class="form-label personal-info-edit news-font" for="inputEmail" style="float: right;"><a href="">Edit </a></label>
-                    <input type="text" class="form-control p-form-style1 m-t-40" id="inputEmail" placeholder="Address" required>
-                    <input type="text" class="form-control p-form-style1 m-t-40" id="inputEmail" placeholder="City" required>
-                    <input type="text" class="form-control p-form-style1 m-t-40" id="inputEmail" placeholder="Pincode" required>
-                  </div>                
-                </div>
-                  <div class="col-md-2 tablet-none"></div>
-                  <div class="col-lg-5 col-md-6 m-t-80 cus-con">
-                    <div>
+                    <div class="col-md-6">
                       <select class="form-control p-form-style1 m-t-50">
                         <option>Country</option>
                         <option>Pakistan</option>
-                      </select>
+                      </select>                      
+                    </div>
+                    
+                    <div class="col-md-6">
+                      <input type="text" class="form-control p-form-style1 m-t-40" id="inputEmail" placeholder="City" required>                      
+                    </div>
+                    
+                    <div class="col-md-6">                      
                       <select  class="form-control p-form-style1 m-t-40">
                         <option>State</option>
                         <option>Pakistan</option>
                       </select>
-                    </div>               
-                  </div>
-              <!-- </div> -->
-                <div class="personal-btn-sec pad-top-100">
-                  <button type="button" class="btn personal-btn1 news-font">SAVE</button>
-                  <button type="button" class="btn personal-btn2 news-font">CANCEL</button>
+                    </div>
+                    
+                    <div class="col-md-6">
+                      <input type="text" class="form-control p-form-style1 m-t-40" id="inputEmail" placeholder="Pincode" required>
+                    </div>
+                  </div>                                  
                 </div>
+
+                <div class="col-lg-6 col-md-6 pad-left-50">
+                  <label class="form-label shipping-info-label news-font" for="inputEmail">SHIPPING METHOD</label>
+                  <div class="shipping-info-content">
+                    <img src="{{URL::to('/public/website')}}/images/checkout-shipping.png">
+                  </div>
+
+                  <label class="form-label shipping-info-label news-font pad-top-40" for="inputEmail">PAYMENT METHOD</label>
+                  <div class="shipping-info-content">
+                    <img src="{{URL::to('/public/website')}}/images/checkout-method.png">
+                  </div>
+
+                  <label class="form-label shipping-info-label news-font pad-top-40" for="inputEmail">PAYMENT METHOD</label>
+                  <div class="shipping-info-content1">
+                    <div class="row">
+                      <div class="col-md-9">
+                        <input type="text" style="border:none; padding-left: 20px; padding-top: 25px;" class="form-control p-form-style1 news-font" id="inputEmail" placeholder="Gift Voucher" required>       
+                      </div>
+                      <div class="col-md-3">
+                         <a href="" class="btn news-font discount-btn">ADD</a>
+                      </div>                      
+                    </div>
+                  </div>
+
+                  <div class="shipping-total pad-top-30">
+                    <h2 class="news-font no-margin">Total</h2>
+                    <div class="row pad-top-20">
+                      <div class="col-md-6"> 
+                        <h3 class="news-font no-margin">Subtotal</h3>
+                      </div>
+                      <div class="col-md-6"> 
+                        <h4 class="no-margin">$1000</h4>
+                      </div>
+                      <div class="col-md-6"> 
+                        <h3 class="news-font no-margin">Delivery</h3>
+                      </div>
+                      <div class="col-md-6"> 
+                        <h4 class="no-margin"><b>Free</b></h4>
+                      </div>
+                    </div>
+                      <hr class="shipping-dash">
+                    <div class="row">
+                      <div class="col-md-6"> 
+                        <h3 class="news-font no-margin">Amount To Pay</h3>
+                      </div>
+                      <div class="col-md-6"> 
+                        <h4 class="no-margin">$1000</h4>
+                      </div>
+                      <div class="col-md-12 pad-top-50"> 
+                        <input type="checkbox" name=""><span class="news-font"> I have read and accepted the terms and conditions.</span>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="checkout-btn-sec pad-top-70">
+                  <button type="button" class="btn checkout-btn news-font">Place Order</button>
+                </div>
+                </div>
+                
               </div>              
             </form>
           </div>
--- resources/views/web/modal.blade.php
+++ resources/views/web/modal.blade.php
@@ -54,11 +54,11 @@
           <div class="container-fluid">
             <div class="row">
               <div class="col-lg-4 col-md-12 col-12" style="background: #044372; text-align: center;">
+                <button type="button" class="close mobile" data-dismiss="modal">&times;</button>
                 <div class="register-section1 pad-top-180">
                   <img src="{{URL::to('/public/website')}}/images/login-page.png">
                   <h3 class="no-margin news-font pad-top-30">CRATE YOUR ACCOUNT</h3>
-                </div>
-                
+                </div>                
               </div>
               <div class="col-lg-8 col-md-12 col-12">
                 <button type="button" class="close" data-dismiss="modal">&times;</button>
@@ -68,12 +68,12 @@
                       <input type="text" class="modal-input" name="" placeholder="Name">
                       <input type="email" class="modal-input" name="" placeholder="Email">
                       <input type="password" class="modal-input" name="" placeholder="Password">
-                      <input type="submit" class="modal-submit" name="" value="Create Account">
+                      <input type="submit" class="modal-submit" style="margin:0px;" name="" value="Create Account">
                     </form>
                   </div>
-                  <div class="login-section3">
+                  <div class="login-section3 create-section3 pad-bot-90">
                     <div class="social-login">
-                      <a href="" style="background: #3B5998;"><i class="fa fa-facebook-f"></i>&nbsp; FACEBOOK</a>
+                      <a href="" class="m-r-30" style="background: #3B5998;"><i class="fa fa-facebook-f"></i>&nbsp; FACEBOOK</a>
                       <a href="" style="background:#D34836;"><i class="fa fa-google"></i>&nbsp; GOOGLE</a>
                     </div>
                   </div>
@@ -94,31 +94,58 @@
         <div class="modal-body login-body">
           <div class="container-fluid">
             <div class="row">
-              <div class="col-md-4" style="background: #044372;">
-                <div class="forget-section1">
-                  <img src="images/login-page.png">
-                  <h3>recover password</h3>
-                </div>
-                
+              <div class="col-lg-4 col-md-12 col-12" style="background: #044372; text-align: center;">
+                <button type="button" class="close mobile" data-dismiss="modal">&times;</button>
+                <div class="forget-section1 pad-top-160">
+                  <img src="{{URL::to('/public/website')}}/images/login-page.png">
+                  <h3 class="no-margin news-font pad-top-30">RECOVER PASSWORD</h3>
+                </div>                
               </div>
               <div class="col-md-8">
                 <button type="button" class="close" data-dismiss="modal">&times;</button>
-                <div class="forget-section2">
-                  <p>IF YOU FORGOT YOUR PASSWORD, ENTER YOUR E-MAIL AND WE WILL SEND YOU A MESSAGE WITH THE INSTRUCTIONS TO RESET YOUR PASSWORD.</p>
+                <div class="forget-section2 pad-top-70 pad-left-30 pad-right-60">
+                  <p class="no-margin news-font pad-bot-50">If you forgot your password, Enter your E-mail and we will send you a message with the instructions to reset your password.</p>
                   <form>
-                    <input type="email"   name="" placeholder="Email">
-                    <input type="submit" style="margin-right: 6px;"  name="" value="Login">
-                    <input type="submit" style="margin-left: 6px;" class="recover-btn" name="" value="Recover">
+                    <input type="email" name="" class="modal-input" placeholder="Email">
+                    <input type="submit" style="margin-right: 6px;" class="forgot-btn1 news-font"  name="" value="Back">
+                    <input type="submit" style="margin-left: 6px;" class="forgot-btn2 news-font" name="" value="Recover">
                   </form>
                 </div>
                 <div class="login-section3">
-                  <h3>forgot your password?</h3>
-                  <div class="social-login">
-                    <a href="" style="background: #3B5998;"><i class="fa fa-facebook-f"></i>&nbsp; FACEBOOK</a>
-                    <a href="" style="background:#D34836;"><i class="fa fa-google"></i>&nbsp; GOOGLE</a>
-                  </div>
-                  <h4>New to Ontailors ? Create an account</h4>
-                </div>                              
+                    <h3 class="news-font no-margin">Forgot Your Password?</h3>
+                    <div class="social-login">
+                      <a href="" class="m-r-30" style="background: #3B5998;"><i class="fa fa-facebook-f"></i>&nbsp; FACEBOOK</a>
+                      <a href="" style="background:#D34836;"><i class="fa fa-google"></i>&nbsp; GOOGLE</a>
+                    </div>
+                    <h4 class="news-font pad-top-60 pad-bot-20">New To Ontailors ? Create An Account</h4>
+                  </div>                               
+              </div>      
+            </div>            
+          </div>
+        </div>
+      </div>      
+    </div>
+  </div>
+
+  <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#payment-successfull">Payment Successfull</button>
+<style type="text/css">
+  
+</style>
+  <div class="modal fade" id="payment-successfull" role="dialog">
+    <div class="modal-dialog" style="max-width: 650px;">
+      <div class="modal-content">        
+        <div class="modal-body login-body">
+          <div class="container-fluid">
+            <div class="row">
+              <div class="col-md-12">
+                <button type="button" class="close payment-cross" data-dismiss="modal">&times;</button>
+                <div class="payment-section pad-top-60">
+                  <img src="{{URL::to('/public/website')}}/images/green-tick.png">
+                  <h2 class="news-font pad-top-20">PAYMENT SUCCESSFUL</h2>
+                  <form>
+                    <input type="submit" class="payment-section-btn news-font m-t-60 m-b-40" name="" value="VIEW ORDER HISTORY">
+                  </form>
+                </div>                             
               </div>      
             </div>            
           </div>
