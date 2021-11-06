0729f628b9ad50b1bcb39c0e88f9183684ea8867
nabeelrajput1 <nabeel.rajput33322@gmail.com>
2021-11-06 20:03:57 +0500

    multi sidebar added

--- resources/views/admin/includes/sidebar.blade.php
+++ resources/views/admin/includes/sidebar.blade.php
@@ -1,17 +1,68 @@
 <div class="sidebar">
    <div class="sidebar-menu">
-      <ul>
-         <li> <a href="{{URL::to('/admin')}}/index"> <img src="{{URL::to('/public/admin')}}/images/menu-icon1.png"> Dashboard </a> </li>
-         <li> <a href="{{URL::to('/public/admin')}}/"> <img src="{{URL::to('/public/admin')}}/images/menu-icon2.png"> Users </a> </li>
-         <li> <a href="{{URL::to('/public/admin')}}/"> <img src="{{URL::to('/public/admin')}}/images/menu-icon3.png"> Orders </a> </li>
-         <li> <a href="{{URL::to('/admin')}}/products"> <img src="{{URL::to('/public/admin')}}/images/menu-icon4.png"> Product </a> </li>
-         <li> <a href="{{URL::to('/admin')}}/fabric"> <img src="{{URL::to('/public/admin')}}/images/menu-icon5.png"> Fabric </a> </li>
-         <li> <a href="{{URL::to('/admin')}}/customization"> <img src="{{URL::to('/public/admin')}}/images/menu-icon6.png"> Customize </a> </li>
-         <li> <a href="{{URL::to('/admin')}}/discount"> <img src="{{URL::to('/public/admin')}}/images/menu-icon7.png"> Discount Coupons </a> </li>
-         <li> <a href="{{URL::to('/public/admin')}}/"> <img src="{{URL::to('/public/admin')}}/images/menu-icon8.png"> Return & Refund </a> </li>
-         <li> <a href="{{URL::to('/public/admin')}}/"> <img src="{{URL::to('/public/admin')}}/images/menu-icon9.png"> SEO </a> </li>
-         <li> <a href="{{URL::to('/public/admin')}}/"> <img src="{{URL::to('/public/admin')}}/images/menu-icon10.png"> Database Backups </a> </li>
-      </ul>
+      <div id="accordion">
+         <div class="panel panel-default">
+            <div class="panel-heading">
+               <a href="{{URL::to('/admin')}}/index"> <img src="{{URL::to('/public/admin')}}/images/menu-icon1.png"> Dashboard </a>
+            </div>
+         </div>
+         <div class="panel panel-default">
+            <div class="panel-heading">
+               <a href="{{URL::to('/public/admin')}}/"> <img src="{{URL::to('/public/admin')}}/images/menu-icon2.png"> Users </a>
+            </div>
+         </div>
+         <div class="panel panel-default">
+            <div class="panel-heading">
+               <a href="{{URL::to('/public/admin')}}/"> <img src="{{URL::to('/public/admin')}}/images/menu-icon3.png"> Orders </a>
+            </div>
+         </div>
+         <div class="panel panel-default">
+            <div class="panel-heading">
+               <a href="{{URL::to('/admin')}}/products"> <img src="{{URL::to('/public/admin')}}/images/menu-icon4.png"> Product </a>
+            </div>
+         </div>
+         <div class="panel panel-default">
+            <div class="panel-heading">
+               <a href="{{URL::to('/admin')}}/fabric"> <img src="{{URL::to('/public/admin')}}/images/menu-icon5.png"> Fabric </a>
+            </div>
+         </div>
+         <div class="panel panel-default">
+            <div class="panel-heading">
+               <a href="{{URL::to('/admin')}}/customization"> <img src="{{URL::to('/public/admin')}}/images/menu-icon6.png"> Customize </a>
+            </div>
+         </div>
+         <div class="panel panel-default">
+            <div class="panel-heading">
+               <a href="{{URL::to('/admin')}}/discount"> <img src="{{URL::to('/public/admin')}}/images/menu-icon7.png"> Discount Coupons </a> 
+            </div>
+         </div>
+         <div class="panel panel-default">
+            <div class="panel-heading">
+               <a href="{{URL::to('/public/admin')}}/"> <img src="{{URL::to('/public/admin')}}/images/menu-icon8.png"> Return & Refund </a>
+            </div>
+         </div>
+         <div class="panel panel-default">
+            <div class="panel-heading">
+               <a href="{{URL::to('/public/admin')}}/"> <img src="{{URL::to('/public/admin')}}/images/menu-icon9.png"> SEO </a>
+            </div>
+         </div>
+         <div class="panel panel-default">
+            <div class="panel-heading">
+               <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne"  > <img src="{{URL::to('/public/admin')}}/images/menu-icon10.png"> Database Backups <i class="fa fa-caret-down"> </i> </a>
+            </div>
+            <div id="collapseOne" class="panel-collapse collapse">
+               <div class="panel-body">
+                  <div class="sub-drop-menu">
+                     <ul>
+                        <li> <a href=""> <i class="fa fa-caret-right"> </i> Sub Menu 1 </a> </li>
+                        <li> <a href=""> <i class="fa fa-caret-right"> </i> Sub Menu 2 </a> </li>
+                        <li> <a href=""> <i class="fa fa-caret-right"> </i> Sub Menu 3 </a> </li>
+                     </ul>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
    </div>
    <div class="blank-height">
       <span> This is Blank height </span> 
